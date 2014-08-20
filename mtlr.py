c015aa6e611959fc45e187bc3761bc36b465f330
snoumlil <ismael.ouamlil@insa-rouen.fr>
2014-08-20 13:58:23 -0400

    Update mtlr.py

--- mtlr.py
+++ mtlr.py
@@ -17,14 +17,15 @@ from scipy import stats
 # read the weights : np.loadtxt("lol.csv")
 
 
-# Input  filename : the name of the file
-# Output  nd array of covariates
 def covariates(fileName):
+	""" Return an Array of covariates, each row refers to a single patient  """
+	""" covaraites(filename)[i] will be the cov for patient i """
 	liste=[]
 	fichier=open(fileName, 'rb')
 	reader = csv.reader(fichier, delimiter=',')
 	for row in reader:
 		liste.append(row)
+	"""Loop to remove the first column which refers to survival time """
 	for i in xrange(0,shape(liste)[0]):
 		liste[i].pop(0)
 	liste.pop(0)
@@ -35,44 +36,34 @@ def covariates(fileName):
 	return np.array(liste)
 
 
-
-# Compute the zcore of each column
 def standardize(covariates):
+	"""Computes the zscore of each feature in the covariates , it speeds up training"""	
 	a=covariates.transpose()
 	for i in xrange(0,shape(a)[0]):
 		a[i]=stats.zscore(a[i])		
 	return a.transpose()
 
 
-
-
-# Input : filename : the name of the csv file 
-# Output : An array of survival Times	
 def readSurv(fileName):
+	"""Returns an array of survival Times """
+	""" readSurv(fileName)[i] is the survival time of patient i"""
 	return np.genfromtxt(fileName,usecols=(0),skip_header=1,delimiter=',',dtype=None)
 
 
-
-# Inputs: -survivalTimes : Array of survival times 
-#         - nbPoints : number of points we want 
-# Output: a nbPoints lentght array from the 1st to the 100 th percentile of survival times.
-#     timepoints=[t1,t2,.....tnbPoints]
 def timePoints(survivalTimes,nbPoints):
+	"""Creates a nbPoints lenght vector , from the 1st to 100th percentile of Survival times """
 	time=[]
 	for i in arange(0,100,100/nbPoints):
 		time.append((np.percentile(survivalTimes,i)))
 	return array(time)
 
-
-# Inputs :  - survTime : survival Time 
-#           - time : array of timePoints
-# Outputs:  a binary sequence of survival time
 def encode(survTime,timePoints):
+	""" Compute a binary sequence equivalent to the survival time, the sequence has the same lenght as the timePoints vector """
 	return (timePoints>=survTime).astype(int)
 	
 
-# Compute the binary sequence for all survival times
 def computeY(survivalTimes,timePoints):
+	""" Given the survivalTimes and timePoints Vectors, it returns an ndarray ofthe encodings for all patients""" 	
 	nbTime=shape(timePoints)[0]
 	nPatient=shape(survivalTimes)[0]
 	Y=np.zeros((nPatient,nbTime))
@@ -80,17 +71,15 @@ def computeY(survivalTimes,timePoints):
 		Y[i]=encode(survivalTimes[i],timePoints)
 	return Y
 
-# Inputs: -Theta: ndArray (initial weights)
-#         - xi array of covariates for patient i
-# 		  -  k integer
-# Outputs: - The score of the sequence with the event occuring in the interval [tk,tk+1)
+
 def fscore(Theta,b,xi,k):
+	"""Compute the score of a binary sequence with the event occuring in the interval [tk,tk+1) for a patient i with covariates xi=X[i]  """
 	maxMonth=shape(Theta)[0]
 	return (Theta[k+1:maxMonth]*xi).sum()+b[k+1:maxMonth].sum()
 
 
 def tic():
-    #Homemade version of matlab tic and toc functions
+    """To evaluate the computing time"""
     import time
     global startTime_for_tictoc
     startTime_for_tictoc = time.time()
@@ -103,12 +92,9 @@ def toc():
         print "Toc: start time not set"
 
 
-# Inputs weigths=[Theta b]
-#        X : array of covariates
-#        nbTime,nVar,nPatient
-#        c1 and c2 parameters
-# Outputs cost	
+
 def cost(Weights,X,Y,c1,c2,nbTime,nVar,nPatient):
+	""" Compute the cost given Weights=[theta b], covariates X, encoding sequences Y, c1 and c2 parameters, nbTime(lenght of the timepoints vector, nVar(number of features), nPatient(number of examples)) """
 	Weights=Weights.reshape(nbTime,nVar+1)
 	X=X.reshape(nPatient,nVar)
 	b=Weights[:,nVar]
@@ -135,11 +121,9 @@ def cost(Weights,X,Y,c1,c2,nbTime,nVar,nPatient):
 	return (s1+s2-(c1/nPatient)*s3)
 
 
-
-
-# Compute the likelihood of a patient
-def likelihood(theta,b,x,ti,time):
-	y=encode(ti,time)
+def likelihood(theta,b,x,ti,timePoints):
+	""" likelihood of a patient with covariates x, at time ti, given theta and b"""
+	y=encode(ti,timePoints)
 	nbTime=shape(theta)[0]	
 	num=0
 	den=0
@@ -150,8 +134,8 @@ def likelihood(theta,b,x,ti,time):
         return (exp(num)/den)
 
 
-# Compute the likelihood for a censored patient
 def likelihoodCensored(topt,b,x,ti,timePoints):
+	""" likelihood of a censored patient with covariates x, at time ti, given theta and b"""
         [nbTime,nVar]=shape(topt)
         num=0
         den=0
@@ -161,16 +145,16 @@ def likelihoodCensored(topt,b,x,ti,timePoints):
                 den=den+exp(fscore(topt,b,x,s))
         return (num/den)
        
-
-# Input array : An array of time points ; value : a value
-# Output  the index of closest value which is in  the array          	
-def findNearest(array,value):
-    idx=(np.abs(array-value)).argmin()
+         	
+def findNearest(array,v):
+    """ the index of the v closest element in the array  """
+    idx=(np.abs(array-v)).argmin()
     return idx 
 
 
-# cost 2
+
 def cost2(Weights,X,surv,time,nbTime,nVar,nPatient):
+    """ Returns -log likelihoodCensored of all patient"""
     X=X.reshape(nPatient,nVar)
     Weights=Weights.reshape(nbTime,nVar+1)
     b=Weights[:,nVar]
@@ -179,8 +163,6 @@ def cost2(Weights,X,surv,time,nbTime,nVar,nPatient):
     for i in xrange(0,nPatient):
                 s=s+log(likelihoodCensored(t,b,X[i],surv[i]))
     return -s
-
-
 
 
 
@@ -195,8 +177,8 @@ def l2(p,t):
 	return abs(p-t)
 
 
-# predTime
 def predTime(topt,b,x,timePoints):
+	"""Compute survival time for a patient given his covariates """
 	nbTime=shape(timePoints)[0]
 	res=np.zeros(nbTime)
 	for j in xrange(0,nbTime):
@@ -207,19 +189,17 @@ def predTime(topt,b,x,timePoints):
 	return timePoints[res.argmin()]
 
 
-
 def deathTimes(topt,b,X,timePoints):
+	"""Returns an array of predicted survival times for all patients """
 	l=[]	
 	for i in xrange(0,shape(X)[0]):
 		l.append(predTime(topt,b,X[i],timePoints))
 	return array(l)
 
 
-     
 
-
-# error
 def rateError(surv,death,tolerence):
+	"""% of differences between the true survival time array and the predicted one   with a certain tolerence""" 	
 	num=0
 	den=shape(death)[0]
 	for i in arange(0,shape(death)[0]):
@@ -229,16 +209,19 @@ def rateError(surv,death,tolerence):
 
 
 
-# Approximate derivative of the cost
 def costDerW(W,X,Y,c1,c2,nbTime,nVar,nPatient):
+	"""Approximates the derivative of the cost """	
 	W=W.reshape(nbTime,nVar+1)
 	X=X.reshape(nPatient,nVar)
 	dW=np.ones((nbTime,nVar+1))/10000000000
 	return ((cost(W+dW,X,Y,c1,c2,nbTime,nVar,nPatient)-cost(W,X,Y,c1,c2,nbTime,nVar,nPatient))/dW).flatten()
 
 
-# train 
+
 def  trainMtlr(X,surv,nbPatient,nbTimePoints,c1,c2):
+	""" Returns the optimal weights([thetaOpt bOpt])"""
+	"""X are the covariates , surv is the array of survival Times  """
+	"""We are free to set the nbTimePoints and nbPatient parameters  """	
 	X=X[0:nbPatient]
 	surv=surv[0:nbPatient]
 	nVar=shape(X)[1]
@@ -257,8 +240,8 @@ def  trainMtlr(X,surv,nbPatient,nbTimePoints,c1,c2):
 
 
 
-# Compute the error
 def computeError(X,surv,opt,nbPatient,nbTimePoints,tolerence):	
+	"""Given opt (result of the training) it computes the global error) """ 
 	X=X[0:nbPatient]	
 	nVar=shape(X)[1]
 	surv=surv[0:nbPatient]
@@ -269,8 +252,8 @@ def computeError(X,surv,opt,nbPatient,nbTimePoints,tolerence):
 	return rateError(surv,death,tolerence)
 
 
-# predict survival time
 def predict(X,surv,opt,nbPatient,nbTimePoints):
+	""" Returns an array of predicted survival Times """ 
 	nVar=shape(X)[1]
 	X=X[0:nbPatient]
 	surv=surv[0:nbPatient]
@@ -284,8 +267,8 @@ def predict(X,surv,opt,nbPatient,nbTimePoints):
 	return death
 
 
-# Plot the survival
-def plotSurvival(topt,b,X,Y,NbPatient,maxMonth,time):
+def plotSurvival(topt,b,X,Y,NbPatient,maxMonth,timePoints):
+	""" Plot the Survival Times by the timePoints for NbPatients"""	
 	nbTime=shape(topt)[0]
 	nbPatient=shape(X)[0]
 	Time=[]
@@ -295,7 +278,7 @@ def plotSurvival(topt,b,X,Y,NbPatient,maxMonth,time):
 	temp=[]
 	for i in xrange(0,NbPatient):
 		for j in xrange(0,maxMonth):
-			a=likelihoodCensored(topt,b,X[i],j,time)
+			a=likelihoodCensored(topt,b,X[i],j,timePoints)
 			temp.append(a)
 		AllPatient.append(temp)
 		temp=[]
